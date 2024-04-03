a59932e003f81f9308e31519778104f23fa489e0
conor <conorlynam1234@gmail.com>
2024-04-03 17:00:05 +0100

    push

--- src/main/java/com/zinkworks/bountyhuntersurlshortener/UrlShortenerApplication.java
+++ src/main/java/com/zinkworks/bountyhuntersurlshortener/UrlShortenerApplication.java
@@ -1,5 +1,8 @@
 package com.zinkworks.bountyhuntersurlshortener;
 
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.storage.Storage;
+import com.google.cloud.storage.StorageOptions;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,13 +10,30 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 @SpringBootApplication
 @EnableJpaRepositories
 
 public class UrlShortenerApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		SpringApplication.run(UrlShortenerApplication.class, args);
+
+//		String projectId = "url-shortener-417915";
+//
+//		String keyFilePath = "C:\\Users\\ConorLynam\\OneDrive - Zinkworks\\Desktop\\Google Cloud Platform";
+//
+//		GoogleCredentials credentials = GoogleCredentials.fromStream(new FileInputStream(keyFilePath));
+//
+//		Storage storage = StorageOptions.newBuilder()
+//				.setCredentials(credentials)
+//				.build()
+//				.getService();
 	}
+
+
 
 }
