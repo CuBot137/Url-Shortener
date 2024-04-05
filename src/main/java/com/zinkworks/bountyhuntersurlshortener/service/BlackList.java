87b7ef6c4f429828dced92fa85704c90a5c3cd86
conor <conorlynam1234@gmail.com>
2024-04-05 14:02:36 +0100

    Docker

--- src/main/java/com/zinkworks/bountyhuntersurlshortener/service/BlackList.java
+++ src/main/java/com/zinkworks/bountyhuntersurlshortener/service/BlackList.java
@@ -2,28 +2,34 @@ package com.zinkworks.bountyhuntersurlshortener.service;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class BlackList {
 
-public static boolean checkBlackList(String originalURL) throws FileNotFoundException{
-    List<String> blackList = new ArrayList<>();
-    Scanner scnr = new Scanner(new FileReader("src/main/resources/keywordsForBlacklist.txt"));
-    String str;
-    while (scnr.hasNext()) {
-        str = scnr.next();
-        blackList.add(str);
-    }
-    for (String substring : blackList) {
-        if (originalURL.contains(substring)) {
-            return true;
+    public static boolean checkBlackList(String originalURL) throws FileNotFoundException {
+        List<String> blackList = new ArrayList<>();
+        // Load the file from the classpath
+        InputStream inputStream = BlackList.class.getResourceAsStream("/keywordsForBlacklist.txt");
+        if (inputStream == null) {
+            throw new FileNotFoundException("keywordsForBlacklist.txt not found in classpath");
         }
-    }
-    return false;
-}
+        Scanner scanner = new Scanner(inputStream);
+        while (scanner.hasNext()) {
+            String str = scanner.next();
+            blackList.add(str);
+        }
+        scanner.close();
 
+        for (String substring : blackList) {
+            if (originalURL.contains(substring)) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
 
 
