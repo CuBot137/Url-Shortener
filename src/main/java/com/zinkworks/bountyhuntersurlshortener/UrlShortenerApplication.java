641c34ca86e816dce8d6aee35c4835480455ef5d
conor <conorlynam1234@gmail.com>
2024-05-08 18:41:41 +0100

    initial commit

--- src/main/java/com/zinkworks/bountyhuntersurlshortener/UrlShortenerApplication.java
+++ src/main/java/com/zinkworks/bountyhuntersurlshortener/UrlShortenerApplication.java
@@ -27,8 +27,6 @@ public class UrlShortenerApplication {
 	public static void main(String[] args) throws IOException {
 		SpringApplication.run(UrlShortenerApplication.class, args);
 
-//gf
-
 	}
 	
 }
