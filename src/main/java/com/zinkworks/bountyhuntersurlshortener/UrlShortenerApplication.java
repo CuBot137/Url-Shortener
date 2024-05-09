2d023c6dc22046c6951616116d58835790ffa303
conor <conorlynam1234@gmail.com>
2024-05-09 09:12:11 +0100

    sql admin

--- src/main/java/com/zinkworks/bountyhuntersurlshortener/UrlShortenerApplication.java
+++ src/main/java/com/zinkworks/bountyhuntersurlshortener/UrlShortenerApplication.java
@@ -26,6 +26,6 @@ public class UrlShortenerApplication {
 
 	public static void main(String[] args) throws IOException {
 		SpringApplication.run(UrlShortenerApplication.class, args);
-		//test1234567
+		//test12345678
 	}
 }
